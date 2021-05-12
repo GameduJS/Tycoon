be54694ef2755a6fc38dfb86ded32913f8a2e1bd
GameduJS <Sagich@dir.net>
2021-05-12 08:22:54 +0200

    changed format (test)

--- TycoonPlugin/src/de/tycoon/discord/commands/ImageCreator.java
+++ TycoonPlugin/src/de/tycoon/discord/commands/ImageCreator.java
@@ -28,10 +28,10 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
-public class ImageCreator implements DiscordCommand{
+public class ImageCreator implements DiscordCommand {
 
 	private File file;
-	
+
 	private TycoonPlugin plugin;
 	private GeneratorManager generatorManager;
 	private UserManager userManager;
@@ -40,7 +40,7 @@ public class ImageCreator implements DiscordCommand{
 		this.plugin = TycoonPlugin.get();
 		this.generatorManager = this.plugin.getGeneratorManager();
 		this.userManager = this.plugin.getUserManager();
-		
+
 		this.file = new File(Config.DISCORD_PATH + "/PictureChache");
 		FileUtils.createFolderIfNotExists(file);
 	}
@@ -56,58 +56,51 @@ public class ImageCreator implements DiscordCommand{
 		// Create a graphics which can be used to draw into the buffered image
 		Graphics2D g2d = bufferedImage.createGraphics();
 
-		//Background
-		g2d.drawImage(
-				ImageIO.read(new File(
-						this.file, "BG.png")),
-				0, 0, width, height, null);
+		// Background
+		g2d.drawImage(ImageIO.read(new File(this.file, "BG.png")), 0, 0, width, height, null);
 
-		//Draw Strings
+		// Draw Strings
 		g2d.setColor(Color.WHITE);
 		g2d.setFont(new Font("Minecraft", 0, 48));
 
 		g2d.drawString(player.getName(), 350, 110);
-		
-		User user = this.userManager
-				.loadUser(
-						player.getUniqueId());
-		
-		String placedGens = "Placed Generators: " + this.generatorManager.getQuantityofGenerators(player.getUniqueId()) + "/" + this.generatorManager.getMaxGens();
-		String money = "Money: " +  new DecimalFormat("#,###.#").format(user.getBalance()) + "€";
+
+		User user = this.userManager.loadUser(player.getUniqueId());
+
+		String placedGens = "Placed Generators: " + this.generatorManager.getQuantityofGenerators(player.getUniqueId())
+				+ "/" + this.generatorManager.getMaxGens();
+		String money = "Money: " + new DecimalFormat("#,###.#").format(user.getBalance()) + "€";
 		String tokens = "Tokens: " + new DecimalFormat("#,###.#").format(user.getTokens());
 		String levels = "Level: " + this.userManager.loadUser(player.getUniqueId()).getUserLevel().getLevel();
-	
+
 		String credits = "Skins by Crafatar";
-		
+
 		g2d.setColor(Color.yellow);
 		g2d.drawString(placedGens, 350, 230);
 		g2d.drawString(money, 350, 230 + g2d.getFont().getSize() + 10);
 		g2d.drawString(tokens, 350, 230 + g2d.getFont().getSize() * 2 + 20);
 		g2d.drawString(levels, 350, 230 + (g2d.getFont().getSize() + 10) * 3);
-		
+
 		g2d.setFont(new Font("Minecraft", 0, 14));
 		g2d.setColor(Color.WHITE);
 		g2d.drawString(credits, width - (credits.length() * g2d.getFont().getSize() / 2) - 10, height - 20);
-		
+
 		// Output -> Calculate on load / add / remove
 		// e.g.: 40$/h
 
 		// Skin
 		String uuid = player.getUniqueId().toString();
-		
-		g2d.drawImage(
-				ImageIO.read(
-						new URL("https://crafatar.com/renders/body/" + uuid + "?scale=10")),
-				100, 50, null);
+
+		g2d.drawImage(ImageIO.read(new URL("https://crafatar.com/renders/body/" + uuid + "?scale=10")), 100, 50, null);
 
 		g2d.dispose();
 
 		// Save as PNG
-		File file = new File(this.file, uuid +".png");
+		File file = new File(this.file, uuid + ".png");
 		ImageIO.write(bufferedImage, "png", file);
-		
+
 		return file;
-		
+
 	}
 
 	@Override
@@ -115,27 +108,30 @@ public class ImageCreator implements DiscordCommand{
 
 		String[] args = message.getContentDisplay().split(" ");
 		message.delete().queue();
-		
-		if(args.length < 2) {
-			textChannel.sendMessage(new EmbedBuilder().setDescription("Hey" + member.getAsMention() + "\nYou have to provide an player name!").setColor(new Color(252, 255, 87)).build()).queue();
+
+		if (args.length < 2) {
+			textChannel.sendMessage(new EmbedBuilder()
+					.setDescription("Hey" + member.getAsMention() + "\nYou have to provide an player name!")
+					.setColor(new Color(252, 255, 87)).build()).queue();
 			return false;
 		}
-		
+
 		OfflinePlayer player = BukkitUtils.getOfflinePlayer(args[1]);
-		if(player == null) {
-			textChannel.sendMessage(new EmbedBuilder().setDescription("Hey" + member.getAsMention() + "\nThis player never played before!").setColor(new Color(255, 26, 26)).build()).queue();
+		if (player == null) {
+			textChannel.sendMessage(new EmbedBuilder()
+					.setDescription("Hey" + member.getAsMention() + "\nThis player never played before!")
+					.setColor(new Color(255, 26, 26)).build()).queue();
 			return false;
 		}
-		
+
 		try {
 			File picture = create(player);
 			textChannel.sendFile(picture).queue();
-			org.apache.cassandra.io.util.FileUtils.delete(new File[]{picture});
+			org.apache.cassandra.io.util.FileUtils.delete(new File[] { picture });
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return false;
 	}
 
